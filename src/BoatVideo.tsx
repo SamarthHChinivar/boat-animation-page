b5450b5c8c6fe4188730e9db463825243846b3d5
Samarth H Chinivar <samarthharish321@gmail.com>
2023-06-08 10:39:06 +0530

    Animations added

--- src/BoatVideo.tsx
+++ src/BoatVideo.tsx
@@ -1,6 +1,6 @@
 const BoatVideo = () => {
   return (
-    <video className="h-100vh w-full object-cover" autoPlay muted loop>
+    <video className="h-100vh w-full object-cover animate-clip-from-top-animation" autoPlay muted loop>
         <source src="src/assets/boat-video.mp4" type="video/mp4"/>
     </video>
   )
